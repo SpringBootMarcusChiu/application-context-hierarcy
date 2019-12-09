d562d24917a9911f88f14b5ff16d10ce41c245da
Marcus Chiu <marcuschiu9@gmail.com>
2019-12-08 20:07:14 -0600

    javadocs added

--- src/main/java/com/marcuschiu/example/applicationcontexthierarchy/ctx1/Ctx1Config.java
+++ src/main/java/com/marcuschiu/example/applicationcontexthierarchy/ctx1/Ctx1Config.java
@@ -12,6 +12,19 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:ctx1.properties")
 @EnableAutoConfiguration
 public class Ctx1Config {
+    /**
+     * IS NEEDED EVEN THOUGH @ComponentScan will pick up Ctx1HomeService
+     * when @Autowired IHomeService is used in for example Ctx1Controller, this
+     * cause error:
+     * - An exception occured while executing the Java class. Error creating bean
+     *   with name 'ctx1Controller': Unsatisfied dependency expressed through field
+     *   'homeService'; nested exception is org.springframework.beans.factory.
+     *   NoUniqueBeanDefinitionException: No qualifying bean of type 'com.marcuschiu.
+     *   example.applicationcontexthierarchy.parent.IHomeService' available: expected
+     *   single matching bean but found 2: ctx1HomeService,parentHomeService
+     * Thus, bean definition below removes this duplication bean error
+     * @return
+     */
     @Bean
     public IHomeService homeService() {
         return new Ctx1HomeService();
