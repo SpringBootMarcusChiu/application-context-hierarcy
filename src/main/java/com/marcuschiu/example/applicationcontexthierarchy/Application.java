938c4d321a8e7ee8ac94de03f7f023b3fdf0c1ca
Marcus Chiu <marcuschiu9@gmail.com>
2020-10-21 22:47:21 -0500

    seperated SpringApplication from applicationContext

--- src/main/java/com/marcuschiu/example/applicationcontexthierarchy/Application.java
+++ src/main/java/com/marcuschiu/example/applicationcontexthierarchy/Application.java
@@ -3,6 +3,7 @@ package com.marcuschiu.example.applicationcontexthierarchy;
 import com.marcuschiu.example.applicationcontexthierarchy.ctx1.Ctx1Config;
 import com.marcuschiu.example.applicationcontexthierarchy.ctx2.Ctx2Config;
 import com.marcuschiu.example.applicationcontexthierarchy.parent.ParentConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
@@ -10,11 +11,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
-		ConfigurableApplicationContext appContext = new SpringApplicationBuilder()
+		SpringApplication springApplication = new SpringApplicationBuilder()
 				.parent(ParentConfig.class).web(WebApplicationType.NONE)	// returns the Parent AnnotationConfigApplicationContext
 				.child(Ctx1Config.class).web(WebApplicationType.SERVLET)	// returns the Child1 AnnotationConfigServletWebServerApplicationContext
 				.sibling(Ctx2Config.class).web(WebApplicationType.SERVLET)	// returns the Child2 AnnotationConfigServletWebServerApplicationContext
-				.run(args);
+				.build();
+
+		ConfigurableApplicationContext appContext = springApplication.run(args);
 
 		System.out.println(appContext.getDisplayName());				// org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext@702c436b
 		System.out.println(appContext.getParent().getDisplayName());	// org.springframework.context.annotation.AnnotationConfigApplicationContext@57a3af25
