4462f10a8cfd9db31e5e60092b6cc2c36460bbfd
Marcus Chiu <marcuschiu9@gmail.com>
2020-10-21 22:38:30 -0500

    added more comments

--- src/main/java/com/marcuschiu/example/applicationcontexthierarchy/Application.java
+++ src/main/java/com/marcuschiu/example/applicationcontexthierarchy/Application.java
@@ -5,14 +5,15 @@ import com.marcuschiu.example.applicationcontexthierarchy.ctx2.Ctx2Config;
 import com.marcuschiu.example.applicationcontexthierarchy.parent.ParentConfig;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext appContext = new SpringApplicationBuilder()
-				.parent(ParentConfig.class).web(WebApplicationType.NONE)
-				.child(Ctx1Config.class).web(WebApplicationType.SERVLET)
-				.sibling(Ctx2Config.class).web(WebApplicationType.SERVLET)
+				.parent(ParentConfig.class).web(WebApplicationType.NONE)	// returns the Parent AnnotationConfigApplicationContext
+				.child(Ctx1Config.class).web(WebApplicationType.SERVLET)	// returns the Child1 AnnotationConfigServletWebServerApplicationContext
+				.sibling(Ctx2Config.class).web(WebApplicationType.SERVLET)	// returns the Child2 AnnotationConfigServletWebServerApplicationContext
 				.run(args);
 
 		System.out.println(appContext.getDisplayName());				// org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext@702c436b
